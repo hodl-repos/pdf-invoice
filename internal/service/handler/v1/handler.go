578f6588bf1df19e8d6fcc28570748e8e1ae23eb
Mauracher Simon <simon@mauracher.cc>
2023-02-14 21:06:43 +0100

    added debug-log for generate v1

--- internal/service/handler/v1/handler.go
+++ internal/service/handler/v1/handler.go
@@ -6,11 +6,17 @@ import (
 
 	"github.com/hodl-repos/pdf-invoice/internal/dto"
 	"github.com/hodl-repos/pdf-invoice/pkg/apihelper"
+	"github.com/hodl-repos/pdf-invoice/pkg/logging"
 )
 
 func Handler() apihelper.HandlerFuncWithError {
 	return func(w http.ResponseWriter, r *http.Request) error {
-		// ctx := r.Context()
+		ctx := r.Context()
+		logger := logging.FromContext(ctx)
+
+		logger.Debugln("got request for v1-generate")
+
+		logger.Debugln("deserializing dto")
 
 		//#region unmarshal
 		var request dto.DocumentDto
@@ -20,6 +26,8 @@ func Handler() apihelper.HandlerFuncWithError {
 			return err
 		}
 
+		logger.Debugln("generating pdf")
+
 		//#endregion unmarshal
 
 		pdf, err := Generate(&request)
@@ -27,10 +35,14 @@ func Handler() apihelper.HandlerFuncWithError {
 			return err
 		}
 
+		logger.Debugln("serializing pdf")
+
 		var buf bytes.Buffer
 		if err := pdf.Output(&buf); err != nil {
 			return err
 		}
+
+		logger.Debugln("sending response")
 
 		w.Header().Set("content-type", "application/pdf")
 		w.WriteHeader(http.StatusOK)
